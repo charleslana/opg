1a93f3f652a9f53569d75fa4ed5ce52b6f24d715
Charles <charleslanop@gmail.com>
2022-06-19 13:26:09 -0300

    fix(character repository): query find character

--- core/repository/CharacterRepository.php
+++ core/repository/CharacterRepository.php
@@ -27,7 +27,7 @@ class CharacterRepository
         $database = new Database();
         $result = $database->select('
             SELECT c.*, ac.*, a.level AS accountLevel FROM `character` c
-                LEFT OUTER JOIN (SELECT MAX(level) as level, MAX(npc_battles) as npc_battles, MAX(arena_battles) as arena_battles, MAX(npc_wins) as npc_wins, MAX(arena_wins) as arena_wins
+                LEFT OUTER JOIN (SELECT MAX(level) AS level, MAX(npc_battles) AS npc_battles, MAX(arena_battles) AS arena_battles, MAX(npc_wins) AS npc_wins, MAX(arena_wins) AS arena_wins
                                  FROM account_character WHERE account_id = :accountId LIMIT 1) ac ON (c.id = c.id)
                     LEFT OUTER JOIN account a ON (a.id = :accountId)
                         WHERE c.id = :id
